79ed1258d258015d9f5ace33fefa9f62d6025bd2
Lenovo 15 <information@com1software.com>
2022-09-10 12:20:40 -0400

    another update for G-mouse

--- main.go
+++ main.go
@@ -3,33 +3,62 @@ package main
 import (
 	"fmt"
 	"log"
+	"strconv"
 
 	"github.com/tarm/serial"
 )
 
+func getComPort() string {
+	isport := false
+	port := "COM"
+	max := 20
+	for i := 0; i < max; i++ {
+		port = "COM" + strconv.Itoa(i)
+
+		c := &serial.Config{Name: port, Baud: 9600}
+		isport = true
+		s, err := serial.OpenPort(c)
+		if err != nil {
+			isport = false
+		}
+		if isport {
+			i = max
+			s.Close()
+		}
+	}
+	return port
+
+}
+
 func main() {
 	fmt.Println("hello world    ....")
-	c := &serial.Config{Name: "COM5", Baud: 9600}
-	fmt.Println("testa")
+	port := getComPort()
+	fmt.Printf("Port : %s\n", port)
+	c := &serial.Config{Name: port, Baud: 9600}
 	s, err := serial.OpenPort(c)
 	if err != nil {
-		fmt.Println("Testb")
+		fmt.Println("Unable to open port")
 		log.Fatal(err)
 	}
+	line := ""
+	buf := make([]byte, 1)
+	on := true
+	n := 0
+	for on != false {
+		n, err = s.Read(buf)
+		if err != nil {
+			fmt.Println("Error ")
+			log.Fatal(err)
+		}
+		if string(buf[:n]) != "13" || string(buf[:n]) != "10" {
+			line = line + string(buf[:n])
+		}
+		fmt.Printf("%s", buf[:n])
+		//	cr := bytes.Equal(buf[:n], []byte(13))
+		if string(buf[:n]) == "13" {
+			fmt.Println(line)
+			line = ""
 
-	n, err := s.Write([]byte("test"))
-	if err != nil {
-		fmt.Println("Test0")
-		log.Fatal(err)
+		}
 	}
-
-	buf := make([]byte, 1280)
-	n, err = s.Read(buf)
-	if err != nil {
-		fmt.Println("Test1")
-		log.Fatal(err)
-	}
-	fmt.Println("Start Test...")
-	log.Print("%s", string(buf[:n]))
-	fmt.Println("... End Test")
 }
