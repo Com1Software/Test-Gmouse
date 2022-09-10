58f4ecb5470b3f5258995f41eba9569c85ca88d1
Lenovo 15 <information@com1software.com>
2022-09-10 10:38:53 -0400

    update for G-mouse

--- main.go
+++ main.go
@@ -2,31 +2,34 @@ package main
 
 import (
 	"fmt"
-      "github.com/tarm/serial"
-      "log"
+	"log"
+
+	"github.com/tarm/serial"
 )
 
 func main() {
 	fmt.Println("hello world    ....")
-      c := &serial.Config{Name: "COM7", Baud: 115200}
-      s, err := serial.OpenPort(c)
-      if err != nil {
-              log.Fatal(err)
-      }
+	c := &serial.Config{Name: "COM5", Baud: 9600}
+	fmt.Println("testa")
+	s, err := serial.OpenPort(c)
+	if err != nil {
+		fmt.Println("Testb")
+		log.Fatal(err)
+	}
 
-      n, err := s.Write([]byte("test"))
-      if err != nil {
-              fmt.Println("Test0")
-              log.Fatal(err)
-      }
+	n, err := s.Write([]byte("test"))
+	if err != nil {
+		fmt.Println("Test0")
+		log.Fatal(err)
+	}
 
-      buf := make([]byte, 128)
-      n, err = s.Read(buf)
-      if err != nil {
-               fmt.Println("Test1")
-              log.Fatal(err)
-      }
-       fmt.Println("Start Test...")
-      log.Print("%q", buf[:n])
-      fmt.Println("... End Test")
+	buf := make([]byte, 1280)
+	n, err = s.Read(buf)
+	if err != nil {
+		fmt.Println("Test1")
+		log.Fatal(err)
+	}
+	fmt.Println("Start Test...")
+	log.Print("%s", string(buf[:n]))
+	fmt.Println("... End Test")
 }
